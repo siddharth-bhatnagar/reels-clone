531afc747b09295885b2c2e71906a820fcf9ef38
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-07-16 19:40:02 +0530

    branch init

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
 import AuthProvider from './context/AuthProvider';
 import './styles/App.css';
-
+import Ioa from './poc/Ioa'
 function App() {
   return (
-    <AuthProvider>
-      <Login/>
-      {/* <SignUp/> */}
-    </AuthProvider>
+    // <AuthProvider>
+    //   <Login/>
+    //   {/* <SignUp/> */}
+    // </AuthProvider>
+    <Ioa />
   );
 }
 
