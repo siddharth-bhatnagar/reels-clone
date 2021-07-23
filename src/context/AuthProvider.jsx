c54a0332dea98deff5e995b4ac2187cf3d197656
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-07-23 15:24:02 +0530

    [context] Auth Provider

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { auth } from '../firebase/firebase';
+
+export const AuthContext = React.createContext();
+
+function AuthProvider({ children }) {
+
+    const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
+
+    const login = (email, password) => {
+        return auth.signInWithEmailAndPassword(email, password);
+    }
+
+    const signup = (email, password) => {
+        return auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    const logout = () => {
+        return auth.signOut();
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            setCurrentUser(user);
+            setLoading(false);
+        });
+
+        return () => {
+            unsubscribe();
+        }
+    }, []);
+
+    const contextObject = {
+        currentUser, 
+        login, 
+        signup,
+        logout
+    }
+
+    return (
+        <AuthContext.Provider value={contextObject} >
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthProvider;
