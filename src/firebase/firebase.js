cf954f8859e900e5237b7e380d7479d28178dc95
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-07-23 15:13:42 +0530

    Masking keys

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,15 +2,9 @@ import firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/storage';
 import 'firebase/firestore';
+import firebaseInit from './config';
 
-firebase.initializeApp({
-    apiKey: "AIzaSyA0EjGhwsErLAS2REwinEGifidGnZz_HmI",
-    authDomain: "reels-a503e.firebaseapp.com",
-    projectId: "reels-a503e",
-    storageBucket: "reels-a503e.appspot.com",
-    messagingSenderId: "1001168718926",
-    appId: "1:1001168718926:web:4d27169c91a6bc93b4471f"
-});
+firebase.initializeApp(firebaseInit);
 
 const firestore = firebase.firestore();
 export const auth = firebase.auth();
