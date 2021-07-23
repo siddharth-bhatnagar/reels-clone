bf974852e19554398359c42b49940c96fa8fc96f
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-07-23 23:58:20 +0530

    [Login, PrivateRoute, LoaderScreen]

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useContext } from 'react';
 import { useStyles } from './style';
 import { AuthContext } from '../../context/AuthProvider';
-import { Grid, Hidden, Paper, CardMedia } from '@material-ui/core';
+import { Grid, Hidden, CardMedia, Card, TextField, Button, Typography } from '@material-ui/core';
 import Carousel from 'react-material-ui-carousel';
 import LoadingScreen from '../LoadingScreen/LoadingScreen';
+import { Link } from 'react-router-dom';
 
 function Login() {
 
@@ -15,32 +16,82 @@ function Login() {
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            console.log('Logging in user...');
+            setLoading(true);
+            await login(email, password);
+            console.log("Success");
+            setLoading(false);
+        } catch {
+            setError("Failed to log in")
+            setTimeout(() => setError(''), 2000)
+            setLoading(false)
+        }
+    }
+
     return (
         <div>
-            {/* <Grid container spacing={4} className={classes.mainContainer}>
-                <Hidden xsDown smDown mdDown>
-                    <Grid item xs={6} sm={6} md={6} lg={7}>
-                        <CardMedia
-                            image="https://www.instagram.com/static/images/homepage/home-phones.png/43cc71bb1b43.png"
-                            className={classes.cardMedia}
-                        >
-                            <div className={classes.carouselContainer}>
-                                <Carousel animation="fade" indicators={false} swipe={false} navButtonsAlwaysInvisible={true} autoPlay={true} interval={5000}>
-                                    <img src="https://www.instagram.com/static/images/homepage/screenshot1.jpg/d6bf0c928b5a.jpg" alt="" />
-                                    <img src="https://www.instagram.com/static/images/homepage/screenshot2.jpg/6f03eb85463c.jpg" alt="" />
-                                    <img src="https://www.instagram.com/static/images/homepage/screenshot4.jpg/842fe5699220.jpg" alt="" />
-                                    <img src="https://www.instagram.com/static/images/homepage/screenshot5.jpg/0a2d3016f375.jpg" alt="" />
-                                </Carousel>
+            {loading ? <LoadingScreen /> :
+                <Grid container spacing={4} className={classes.mainContainer}>
+                    <Hidden xsDown smDown mdDown>
+                        <Grid item xs="auto" sm="auto" md="auto" lg={5}>
+                            <div className={classes.leftMediaDiv}>
+                                <CardMedia className={classes.leftMediaContainer} image="https://www.instagram.com/static/images/homepage/home-phones.png/43cc71bb1b43.png">
+                                    <Carousel animation="fade" indicators={false} swipe={false} navButtonsAlwaysInvisible={true} autoPlay interval="5000" className={classes.carousel}>
+                                        <img className={classes.carouselImage} src="https://www.instagram.com/static/images/homepage/screenshot1.jpg/d6bf0c928b5a.jpg" alt="" />
+                                        <img className={classes.carouselImage} src="https://www.instagram.com/static/images/homepage/screenshot2.jpg/6f03eb85463c.jpg" alt="" />
+                                        <img className={classes.carouselImage} src="https://www.instagram.com/static/images/homepage/screenshot4.jpg/842fe5699220.jpg" alt="" />
+                                        <img className={classes.carouselImage} src="https://www.instagram.com/static/images/homepage/screenshot5.jpg/0a2d3016f375.jpg" alt="" />
+                                    </Carousel>
+                                </CardMedia>
                             </div>
-                        </CardMedia>
+                        </Grid>
+                    </Hidden>
+                    <Grid item xs={12} sm={6} md={5} lg={4}>
+                        <Card variant="outlined" className={classes.card}>
+                            <CardMedia image="https://www.logo.wine/a/logo/Instagram/Instagram-Wordmark-Black-Logo.wine.svg" className={classes.cardLogo} />
+                            <Grid container spacing={3}>
+                                <Grid item xs={12} sm={12} md={12} lg={12} >
+                                    <TextField className={classes.textField} variant="outlined" size="small" value={email} onChange={(e) => setEmail(e.target.value)} type="email" label="Email" placeholder="johndoe@example.com" />
+                                </Grid>
+                                <Grid item xs={12} sm={12} md={12} lg={12} >
+                                    <TextField className={classes.textField} variant="outlined" size="small" value={password} onChange={(e) => setPassword(e.target.value)} type="password" label="Password" />
+                                </Grid>
+                                <Grid item xs={12} sm={12} md={12} lg={12} >
+                                    <Button className={classes.btn} disabled={loading} color="primary" size="small" variant="contained" onClick={handleSubmit}>Log In</Button>
+                                </Grid>
+                                <Grid item xs={12} sm={12} md={12} lg={12} >
+                                    <Typography
+                                        style={{ textAlign: "center", fontSize: "14px", marginTop: "10px", color: "#00376B"}}
+                                        variant="body1"
+                                        gutterBottom>
+                                        <LinkButton content="Forgot Password?" routeLink="/signup" />
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Card>
+                        <Card
+                            variant="outlined"
+                            style={{ marginTop: "2rem", width: "15rem", margin: "0.5rem" }}>
+                            <Typography
+                                style={{ textAlign: "center", padding: "1rem", fontSize: "14px", color: "#262626" }}
+                                variant="body1"
+                                gutterBottom>
+                                Don't have an account? <LinkButton content="Sign up" routeLink="/signup" />
+                            </Typography>
+                        </Card>
                     </Grid>
-                </Hidden>
-                <Grid item xs={11} sm={8} md={6} lg={5}>
-                    {loading ? }
                 </Grid>
-            </Grid> */}
-            <LoadingScreen/>
+            }
         </div>
+    );
+}
+
+function LinkButton({ content, routeLink }) {
+    return (
+        <Link style={{ textDecoration: "none", color: "#0095F6", fontSize: "14px" }} to={routeLink}>{content}</Link>
     );
 }
 
--- src/components/Login/style.js
+++ src/components/Login/style.js
@@ -3,23 +3,57 @@ import { makeStyles } from "@material-ui/core";
 export const useStyles = makeStyles({
     mainContainer: {
         height: "100vh",
-        width: "80vw",
+        width: "75vw",
         display: "flex",
+        flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
+        margin: "auto",
+        backgroundColor: "rgba(var(--b3f,250,250,250),1)"
+    },
+    leftMediaDiv: {
+        position: "relative",
+    },
+    leftMediaContainer: {
+        height: "618px",
+        width: "454px",
+        display: "flex"
+    },
+    carousel: {
+        position: "relative",
+        left: "150.5px"
+    },
+    carouselImage: {
+        height: "427px",
+        width: "240px",
+        position: "relative",
+        top: "100px",
+        padding: 0,
+        zIndex: "3"
+    },
+    card: {
+        height: "15rem",
+        width: "15rem",
+        margin: "0.5rem"
+    },
+    cardLogo: {
+        height: "100px",
+        width: "250px",
         margin: "auto"
     },
-    cardMedia: {
-        backgroundSize: "contain",
-        height: "86vh",
-        position: "relative",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
+    textField: {
+        "& input::placeholder": {
+            fontSize: "18px"
+        },
+        marginLeft: "1rem",
+        marginRight: "1rem",
+        height: "40px",
     },
-    carouselContainer: { 
-        height: "26rem", 
-        width: "15rem", 
-        marginLeft: "5.5rem" 
+    btn: {
+        backgroundColor: "#0095F6",
+        marginLeft: "1rem",
+        marginRight: "1rem",
+        width: "312px",
+        height: "35px"
     }
 });
